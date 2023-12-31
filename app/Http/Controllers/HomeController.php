2a3b0f50c7ecb6b65dfc250d1cbae122118a25ac
jltamolo <jltamolo@gmail.com>
2023-12-31 22:05:32 +0300

    front page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,16 @@ use App\Models\User;
 
 class HomeController extends Controller
 {
+    public function index(){
+        return view('home.userpage');
+    }
     public function redirect(){
         $usertype=Auth::user()->usertype;
         if($usertype=='1'){
             return view('admin.home');
         }
         else{
-            return view('dashboard');
+            return view('home.userpage');
         }
     }
 }
